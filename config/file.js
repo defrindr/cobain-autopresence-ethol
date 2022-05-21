b3595c5452d34b20770803a375bfc099c44741b2
Defri Indra M <defrindr@gmail.com>
2022-05-21 09:19:17 +0700

    update: base path :fire:

--- config/file.js
+++ config/file.js
@@ -1,9 +1,11 @@
+let BASE_PATH = process.cwd() + "/";
 let TOKEN_PATH = "./temp/token.txt";
 let CREDENTIALS_PATH = "./credentials.json";
 let ETHOL_CONF_PATH = "./temp/ethol.js";
 let DEBUG_PATH = "./temp/debug.log";
 
 module.exports = {
+    BASE_PATH,
     TOKEN_PATH,
     CREDENTIALS_PATH,
     ETHOL_CONF_PATH,
